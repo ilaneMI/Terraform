5950e7a10b0b1702c5bc5753ea9ad0fbb2e1186e
Ilane Mac-luckie <150701454+ilaneMI@users.noreply.github.com>
2026-02-23 11:16:58 +0100

    feat: add main.tf - 2 petites VM Azure Linux (Standard_B1s)

---
+++ main.tf
@@ -0,0 +1,97 @@
+##############################################
+# Terraform - 2 petites VM Azure (Standard_B1s)
+##############################################
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+########################
+# Resource Group
+########################
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+########################
+# Reseau virtuel
+########################
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+########################
+# Network Interfaces (une par VM)
+########################
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "${var.prefix}-nic-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+########################
+# 2 petites VM Linux Ubuntu
+########################
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = 2
+  name                = "${var.prefix}-vm-${count.index}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    name                 = "${var.prefix}-osdisk-${count.index}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 30
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "dev"
+    managed_by  = "terraform"
+  }
+}
